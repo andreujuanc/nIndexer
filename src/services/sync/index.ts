6c4bfa6712de7e687cbc695010ab9778d25361b8
Juan C. Andreu <andreujuanc@gmail.com>
2022-07-31 14:26:02 +0000

    Cleanup

--- src/services/sync/index.ts
+++ src/services/sync/index.ts
@@ -3,6 +3,7 @@ import { getNextBlock, saveBlock } from "../../data/models/blocks";
 import { saveTransactions } from "../../data/models/transactions";
 import { saveLogs } from "../../data/models/logs";
 import { beginTransaction, commitTransaction, rollbackTransaction } from "../../data/postgress";
+import { PoolClient } from "pg";
 
 
 
@@ -12,7 +13,6 @@ export async function startSync(chainId: number) {
         const client = await beginTransaction()
 
         try {
-
             const nextBlock = await getNextBlock(client)
             if (nextBlock % 100 === 0) {
                 const current = await provider.getBlockNumber()
@@ -20,48 +20,9 @@ export async function startSync(chainId: number) {
             }
             else
                 console.log('ℹ️ Syncing block:', nextBlock)
-            const block = await provider.getBlock(nextBlock)
-            // const txs = await Promise.all(block.transactions.map(async (hash) =>
-            //     provider.getTransactionReceipt(hash)
-            // ))
-            // const block = await provider.getBlockWithTransactions(nextBlock)
-            // const txs = block.transactions
-            const logs = await provider.getLogs({ fromBlock: nextBlock, toBlock: nextBlock })
 
-
-            // if (txs.length > 0) {
-            // await saveTransactions(client, txs.map(x => ({
-            //     ...x,
-            //     txto: x.to,
-            //     txfrom: x.from,
-            //     gasUsed: x.gasUsed.toString(),
-            //     cumulativeGasUsed: x.cumulativeGasUsed.toString(),
-            //     effectiveGasPrice: x.effectiveGasPrice.toString(),
-            // })))
-
-            // const logs = txs.flatMap(x => x.logs.map(l => ({
-            //     ...l,
-            //     topics: JSON.stringify(l.topics),
-            //     removed: l.removed ?? false
-            // })))
-
-            if (logs.length > 0) {
-                await saveLogs(client, logs.flatMap(log => ({
-                    ...log,
-                    topics: JSON.stringify(log.topics),
-                    removed: log.removed ?? false
-                })))
-            }
-            //}
-
-            await saveBlock(client, {
-                number: block.number,
-                data: block
-            })
-
+            await syncBlock(client, provider, nextBlock);
             await commitTransaction(client)
-
-            //await new Promise(resolve => setTimeout(resolve, 100))
         } catch (ex) {
             await rollbackTransaction(client)
             console.error(ex)
@@ -71,4 +32,42 @@ export async function startSync(chainId: number) {
             client.release()
         }
     }
+}
+
+async function syncBlock(client: PoolClient, provider: ethers.providers.StaticJsonRpcProvider, nextBlock: number) {
+    const block = await provider.getBlock(nextBlock);
+    // const txs = await Promise.all(block.transactions.map(async (hash) =>
+    //     provider.getTransactionReceipt(hash)
+    // ))
+    // const block = await provider.getBlockWithTransactions(nextBlock)
+    // const txs = block.transactions
+    const logs = await provider.getLogs({ fromBlock: nextBlock, toBlock: nextBlock });
+
+
+    // if (txs.length > 0) {
+    // await saveTransactions(client, txs.map(x => ({
+    //     ...x,
+    //     txto: x.to,
+    //     txfrom: x.from,
+    //     gasUsed: x.gasUsed.toString(),
+    //     cumulativeGasUsed: x.cumulativeGasUsed.toString(),
+    //     effectiveGasPrice: x.effectiveGasPrice.toString(),
+    // })))
+    // const logs = txs.flatMap(x => x.logs.map(l => ({
+    //     ...l,
+    //     topics: JSON.stringify(l.topics),
+    //     removed: l.removed ?? false
+    // })))
+    if (logs.length > 0) {
+        await saveLogs(client, logs.flatMap(log => ({
+            ...log,
+            topics: JSON.stringify(log.topics),
+            removed: log.removed ?? false
+        })));
+    }
+    //}
+    await saveBlock(client, {
+        number: block.number,
+        data: block
+    });
 }
